02ee16f76cbef7b2a21203b963390b911ccd84f1
jbogner <boginator55@gmail.com>
2021-05-02 00:26:32 -0400

    Add PID control and simulate drone movement to align with face detections.

--- faceTello.go
+++ faceTello.go
@@ -24,20 +24,60 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/image/colornames"
+	"image"
 	"io"
 	"log"
+	"math"
 	"os/exec"
 	"time"
 
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/platforms/dji/tello"
 	"gocv.io/x/gocv"
-	"golang.org/x/image/colornames"
 )
 
 const (
 	frameSize = 960 * 720 * 3
+	MAXWIDTH  = 960 //drone FOV width in pixels
+	MAXHEIGHT = 720
+	CURSORX   = MAXWIDTH / 2
+	CURSORY   = MAXHEIGHT / 2
+	TOOCLOSE  = 200 //Any detected object wider than this width should stop the drone
 )
+
+var left int
+var right int
+var top int
+var bottom int
+var length int
+var height int
+var centerL int
+var centerH int
+var crosshair = image.Point{CURSORX, CURSORY}  //Mark to appear at center of camera
+var tracker = image.Point{CURSORX, CURSORY}    //Mark to stand in as our tracking mark. substitute for movement.
+var correction = image.Point{CURSORX, CURSORY} //This is our simulated movement icon. We'll start it in the center with the crosshair.
+var oldCRC uint16
+var lastNewFrameTime time.Time
+var frameDiff time.Duration
+var timeDiff time.Duration
+var newImage bool
+
+var lastUpdate time.Time
+var lastErrorX float64 = 0
+var lastErrorY float64 = 0
+var changeX float64 = 0
+var changeY float64 = 0
+var derivativeX float64 = 0
+var derivativeY float64 = 0
+var lastIntegralX float64 = 0
+var lastIntegralY float64 = 0
+var KpX = 0.05
+var KpY = 0.05
+var KiX float64 = 0 //0.0005
+var KiY float64 = 0 //0.0005
+var KdX float64 = 0
+var KdY float64 = 0
 
 func main() {
 	drone := tello.NewDriver("8890")
@@ -93,7 +133,6 @@ func main() {
 			fmt.Println(err)
 			continue
 		}
-
 		img, err := gocv.NewMatFromBytes(720, 960, gocv.MatTypeCV8UC3, buf)
 		if err != nil {
 			log.Print(err)
@@ -102,12 +141,39 @@ func main() {
 		if img.Empty() {
 			continue
 		}
-		imageRectangles := classifier.DetectMultiScale(img)
-
-		for _, rect := range imageRectangles {
-			log.Println("found a face,", rect)
-			gocv.Rectangle(&img, rect, colornames.Cadetblue, 3)
+		newCRC := tello.CalculateCRC16(buf)
+		if newCRC != oldCRC {
+			oldCRC = newCRC
+			current := time.Now()
+			frameDiff = current.Sub(lastNewFrameTime)
+			lastNewFrameTime = time.Now()
+			//fmt.Println("New image! Time since last frame: ", frameDiff)
+			newImage = true
+		} else {
+			//fmt.Println("Old image!")
+			newImage = false
 		}
+		gocv.Circle(&img, crosshair, 8, colornames.Magenta, 3) //Show the drone crosshair
+		if newImage {
+			imageRectangles := classifier.DetectMultiScale(img)
+			for _, rect := range imageRectangles {
+				length = right - left
+				height = bottom - top
+				centerL = left + (length / 2)
+				centerH = bottom + (height / 2)
+				center := getCenter(rect)
+				gocv.Rectangle(&img, rect, colornames.Cadetblue, 3)          //Box our detection
+				gocv.Circle(&img, center, 5, colornames.Cadetblue, 3)        //Mark the center of the detection
+				gocv.Line(&img, center, crosshair, colornames.Aquamarine, 3) //Draw a line from center to crosshair
+				//HERE WE WANT TO MOVE OUR TRACKER TOWARDS THE DETECTION USING PID CONTROL ALGORITHM
+				newCorrection, movement := getTargetOutput(center, correction) //Run our error checking algorithm to determine where to go
+				correction = newCorrection
+				gocv.Circle(&img, newCorrection, 10, colornames.Midnightblue, 3) //Mark the simulated movement to the detection
+				handleMovement(movement, *drone)
+				//log.Println("found a face,", rect, "of size ", rect.Size(), "with center ", center, "at ", time.Now())
+			}
+		}
+
 		window.IMShow(img)
 		if window.WaitKey(1) >= 0 {
 			break
@@ -118,4 +184,62 @@ func main() {
 		//window.WaitKey(1)
 		//count +=1
 	}
+}
+
+func getCenter(rect image.Rectangle) image.Point {
+	cornerA := image.Point{rect.Max.X, rect.Max.Y}
+	cornerB := image.Point{rect.Min.X, rect.Min.Y}
+	rectWidth := cornerB.X + (rect.Dx() / 2)
+	rectHeight := cornerA.Y - (rect.Dy() / 2)
+	return image.Point{rectWidth, rectHeight}
+}
+
+func getTargetOutput(target image.Point, tracker image.Point) (image.Point, image.Point) {
+	currentTime := time.Now()
+	timeDiff := currentTime.Sub(lastUpdate).Milliseconds()
+	diffX := float64(target.X - tracker.X)
+	diffY := float64(target.Y - tracker.Y)
+	changeX = lastErrorX - diffX
+	changeY = lastErrorY - diffY
+	fmt.Println("changeX", changeX)
+	fmt.Println("changeY", changeY)
+	integralX := lastIntegralX + diffX*float64(timeDiff)
+	integralY := lastIntegralY + diffY*float64(timeDiff)
+	derivativeX = (diffX - lastErrorX) / float64(timeDiff)
+	derivativeY = (diffY - lastErrorY) / float64(timeDiff)
+	velX := (KpX * diffX) + (KiX * integralX) //+(KdX*derivativeX)
+	velY := (KpY * diffY) + (KiY * integralY) //+(KdY*derivativeY)
+	newX := float64(tracker.X) + velX
+	newY := float64(tracker.Y) + velY
+	lastUpdate = time.Now()
+	lastErrorX = diffX
+	lastErrorY = diffY
+	lastIntegralX = integralX
+	lastIntegralY = integralY
+	fmt.Println("velX", velX, "velY", velY, "newX", newX, "newY", newY)
+	return image.Point{X: int(newX), Y: int(newY)}, image.Point{X: int(velX), Y: int(velY)}
+}
+
+func handleMovement(target image.Point, drone tello.Driver) {
+	if target.X > 0 {
+		xval := int(math.Abs(float64(target.X)))
+		drone.Left(xval)
+	} else if target.X < 0 {
+		xval := int(math.Abs(float64(target.X)))
+		drone.Right(xval)
+	} else {
+		drone.Hover()
+	}
+	if target.Y > 0 {
+		yval := int(math.Abs(float64(target.Y)))
+		drone.Up(yval)
+	} else if target.Y < 0 {
+		yval := int(math.Abs(float64(target.Y)))
+		drone.Down(yval)
+	} else {
+		drone.Hover()
+	}
+	//ADD CODE: IF detection is to small, move forward a bit
+	//else if detection size OK, HOVER
+	//IF detection size exceeds threshold, LAND
 }
